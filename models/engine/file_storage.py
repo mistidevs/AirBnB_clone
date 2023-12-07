10bf62cfcd594d131e196ed2910215bd649566f8
ZHAYRI123 <merykozan@gmail.com>
2023-12-07 14:42:28 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,3 +8,11 @@ class FileStorage:
     """serializes instances to a JSON file and deserializes it"""
     __file_path = "file.json"
     __objects = {}
+
+    def __init__(self):
+        """mothode initializing"""
+        pass
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return (self.__objects)

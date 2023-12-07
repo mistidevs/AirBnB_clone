1b1b3cb70bb23fb67270227afce94bfa07fa145a
ZHAYRI123 <merykozan@gmail.com>
2023-12-07 15:13:42 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,3 +21,13 @@ class FileStorage:
         """sets in __objects the obj with key <obj class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        seri_dict = {}
+        for key, value in self.__objects:
+            seri_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(seri_dict, f)
+
+

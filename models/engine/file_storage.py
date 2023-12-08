4c108f314c634762d242dc79037aea13ce727e2a
ZHAYRI123 <merykozan@gmail.com>
2023-12-08 03:29:13 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python3
-"""Def of FileStorage class"""
-
-
+"""FileStorage class that stores models in JSON format for persistence"""
+import copy
 import json
+import datetime
+from models.base_model import BaseModel
+from models.user import User
+
+
+def str_to_datetime(s):
+    """Convert a string in ISO format to a datetime object"""
+    return datetime.datetime.fromisoformat(s)i
 
 class FileStorage:
-    """serializes instances to a JSON file and deserializes it"""
-    __file_path = "file.json"
+    """The nitty gritties of how it does this"""
     __objects = {}
+    __file_path = "file.json"
 
-    def __init__(self):
-        """mothode initializing"""
-        pass
+     @property
+    def objects(self):
+        """Return the __objects dictionary"""
+        return self.__objects
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return (self.__objects)
+     def all(self):
+        """Return all the objects made"""
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        """Adding a new instance"""
+        key = type(obj).__name__ + "." + obj.id
         self.__objects[key] = obj
 
-    def save(self):
-        """serializes __objects to the JSON file"""
-        seri_dict = {}
-        for key, value in self.__objects:
-            seri_dict[key] = value.to_dict()
-        with open(self.__file_path, "w") as f:
-            json.dump(seri_dict, f)
+     def save(self):
+        """Saving all the objects to a JSON file"""
+        json_opt = {}
+        for key, value in self.__objects.items():
+            json_opt[key] = value.to_dict()
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(json_opt, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """Converting JSON file to objects"""
         try:
-            with open(self.__file_path, 'r') as f:
-               di = json.load(f)
+            with open(self.__file_path, mode="r", encoding="utf-8") as f:
+                json_dict = json.load(f)
+            for key, value in json_dict.items():
+                cls_name = value["__class__"]
+                cls = eval(cls_name)
+                self.__objects[key] = cls(**value)
+
         except FileNotFoundError:
             pass
-

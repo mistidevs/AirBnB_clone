21ef1954aaab590bdbc94caa1cde9809cf2478a2
mistidevs <mistidevs@gmail.com>
2023-12-08 17:45:43 +0300

    Indentation

--- console.py
+++ console.py
@@ -191,159 +191,159 @@ class HBNBCommand(cmd.Cmd):
                     self.do_update(command)
 
         def do_State(self, arg):
-        if arg == ".all()":
-            self.do_all("State")
-        elif arg == ".count()":
-            counter = 0
-            for key in storage.objects:
-                args = key.split(".")
-                if args[0] == "State":
-                    counter += 1
-            print(counter)
-        else:
-            parts = arg.split("(")
-            cmd = parts[0]
-            if cmd == ".show":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "State " + argi
-                self.do_show(command)
-            elif cmd == ".destroy":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "State " + argi
-                self.do_destroy(command)
-            elif cmd == ".update":
-                args = parts[1].split(", ")
-                arg_1 = args[0].split("\"")[1]
-                arg_2 = args[1].split("\"")[1]
-                arg_3 = args[2].split("\"")[1]
-                command = "State " + arg_1 + " " + arg_2 + " " + arg_3
-                self.do_update(command)
+            if arg == ".all()":
+                self.do_all("State")
+            elif arg == ".count()":
+                counter = 0
+                for key in storage.objects:
+                    args = key.split(".")
+                    if args[0] == "State":
+                        counter += 1
+                print(counter)
+            else:
+                parts = arg.split("(")
+                cmd = parts[0]
+                if cmd == ".show":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "State " + argi
+                    self.do_show(command)
+                elif cmd == ".destroy":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "State " + argi
+                    self.do_destroy(command)
+                elif cmd == ".update":
+                    args = parts[1].split(", ")
+                    arg_1 = args[0].split("\"")[1]
+                    arg_2 = args[1].split("\"")[1]
+                    arg_3 = args[2].split("\"")[1]
+                    command = "State " + arg_1 + " " + arg_2 + " " + arg_3
+                    self.do_update(command)
 
         def do_City(self, arg):
-        if arg == ".all()":
-            self.do_all("City")
-        elif arg == ".count()":
-            counter = 0
-            for key in storage.objects:
-                args = key.split(".")
-                if args[0] == "City":
-                    counter += 1
-            print(counter)
-        else:
-            parts = arg.split("(")
-            cmd = parts[0]
-            if cmd == ".show":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "City " + argi
-                self.do_show(command)
-            elif cmd == ".destroy":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "City " + argi
-                self.do_destroy(command)
-            elif cmd == ".update":
-                args = parts[1].split(", ")
-                arg_1 = args[0].split("\"")[1]
-                arg_2 = args[1].split("\"")[1]
-                arg_3 = args[2].split("\"")[1]
-                command = "City " + arg_1 + " " + arg_2 + " " + arg_3
-                self.do_update(command)
+            if arg == ".all()":
+                self.do_all("City")
+            elif arg == ".count()":
+                counter = 0
+                for key in storage.objects:
+                    args = key.split(".")
+                    if args[0] == "City":
+                        counter += 1
+                print(counter)
+            else:
+                parts = arg.split("(")
+                cmd = parts[0]
+                if cmd == ".show":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "City " + argi
+                    self.do_show(command)
+                elif cmd == ".destroy":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "City " + argi
+                    self.do_destroy(command)
+                elif cmd == ".update":
+                    args = parts[1].split(", ")
+                    arg_1 = args[0].split("\"")[1]
+                    arg_2 = args[1].split("\"")[1]
+                    arg_3 = args[2].split("\"")[1]
+                    command = "City " + arg_1 + " " + arg_2 + " " + arg_3
+                    self.do_update(command)
 
         def do_Amenity(self, arg):
-        if arg == ".all()":
-            self.do_all("Amenity")
-        elif arg == ".count()":
-            counter = 0
-            for key in storage.objects:
-                args = key.split(".")
-                if args[0] == "Amenity":
-                    counter += 1
-            print(counter)
-        else:
-            parts = arg.split("(")
-            cmd = parts[0]
-            if cmd == ".show":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Amenity " + argi
-                self.do_show(command)
-            elif cmd == ".destroy":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Amenity " + argi
-                self.do_destroy(command)
-            elif cmd == ".update":
-                args = parts[1].split(", ")
-                arg_1 = args[0].split("\"")[1]
-                arg_2 = args[1].split("\"")[1]
-                arg_3 = args[2].split("\"")[1]
-                command = "Amenity " + arg_1 + " " + arg_2 + " " + arg_3
-                self.do_update(command)
+            if arg == ".all()":
+                self.do_all("Amenity")
+            elif arg == ".count()":
+                counter = 0
+                for key in storage.objects:
+                    args = key.split(".")
+                    if args[0] == "Amenity":
+                        counter += 1
+                print(counter)
+            else:
+                parts = arg.split("(")
+                cmd = parts[0]
+                if cmd == ".show":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Amenity " + argi
+                    self.do_show(command)
+                elif cmd == ".destroy":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Amenity " + argi
+                    self.do_destroy(command)
+                elif cmd == ".update":
+                    args = parts[1].split(", ")
+                    arg_1 = args[0].split("\"")[1]
+                    arg_2 = args[1].split("\"")[1]
+                    arg_3 = args[2].split("\"")[1]
+                    command = "Amenity " + arg_1 + " " + arg_2 + " " + arg_3
+                    self.do_update(command)
 
         def do_Place(self, arg):
-        if arg == ".all()":
-            self.do_all("Place")
-        elif arg == ".count()":
-            counter = 0
-            for key in storage.objects:
-                args = key.split(".")
-                if args[0] == "Place":
-                    counter += 1
-            print(counter)
-        else:
-            parts = arg.split("(")
-            cmd = parts[0]
-            if cmd == ".show":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Place " + argi
-                self.do_show(command)
-            elif cmd == ".destroy":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Place " + argi
-                self.do_destroy(command)
-            elif cmd == ".update":
-                args = parts[1].split(", ")
-                arg_1 = args[0].split("\"")[1]
-                arg_2 = args[1].split("\"")[1]
-                arg_3 = args[2].split("\"")[1]
-                command = "Place " + arg_1 + " " + arg_2 + " " + arg_3
-                self.do_update(command)
+            if arg == ".all()":
+                self.do_all("Place")
+            elif arg == ".count()":
+                counter = 0
+                for key in storage.objects:
+                    args = key.split(".")
+                    if args[0] == "Place":
+                        counter += 1
+                print(counter)
+            else:
+                parts = arg.split("(")
+                cmd = parts[0]
+                if cmd == ".show":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Place " + argi
+                    self.do_show(command)
+                elif cmd == ".destroy":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Place " + argi
+                    self.do_destroy(command)
+                elif cmd == ".update":
+                    args = parts[1].split(", ")
+                    arg_1 = args[0].split("\"")[1]
+                    arg_2 = args[1].split("\"")[1]
+                    arg_3 = args[2].split("\"")[1]
+                    command = "Place " + arg_1 + " " + arg_2 + " " + arg_3
+                    self.do_update(command)
 
         def do_Review(self, arg):
-        if arg == ".all()":
-            self.do_all("Review")
-        elif arg == ".count()":
-            counter = 0
-            for key in storage.objects:
-                args = key.split(".")
-                if args[0] == "Review":
-                    counter += 1
-            print(counter)
-        else:
-            parts = arg.split("(")
-            cmd = parts[0]
-            if cmd == ".show":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Review " + argi
-                self.do_show(command)
-            elif cmd == ".destroy":
-                args = parts[1].split("\"")
-                argi = args[1]
-                command = "Review " + argi
-                self.do_destroy(command)
-            elif cmd == ".update":
-                args = parts[1].split(", ")
-                arg_1 = args[0].split("\"")[1]
-                arg_2 = args[1].split("\"")[1]
-                arg_3 = args[2].split("\"")[1]
-                command = "Review " + arg_1 + " " + arg_2 + " " + arg_3
-                self.do_update(command)
+            if arg == ".all()":
+                self.do_all("Review")
+            elif arg == ".count()":
+                counter = 0
+                for key in storage.objects:
+                    args = key.split(".")
+                    if args[0] == "Review":
+                        counter += 1
+                print(counter)
+            else:
+                parts = arg.split("(")
+                cmd = parts[0]
+                if cmd == ".show":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Review " + argi
+                    self.do_show(command)
+                elif cmd == ".destroy":
+                    args = parts[1].split("\"")
+                    argi = args[1]
+                    command = "Review " + argi
+                    self.do_destroy(command)
+                elif cmd == ".update":
+                    args = parts[1].split(", ")
+                    arg_1 = args[0].split("\"")[1]
+                    arg_2 = args[1].split("\"")[1]
+                    arg_3 = args[2].split("\"")[1]
+                    command = "Review " + arg_1 + " " + arg_2 + " " + arg_3
+                    self.do_update(command)
                 
         
     def do_EOF(self, line):

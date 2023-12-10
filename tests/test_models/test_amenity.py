0b19620aaa505975ff2fd597cae2ffa206476132
ZHAYRI123 <merykozan@gmail.com>
2023-12-10 10:21:31 -0800

    update

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Def the unittest for the Amenity class"""
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+import os
+from models.place import Amenity
+
+
+class Testamenity_values(unittest.TestCase):
+    """Test the aminity class values"""
+
+    def test_no_args(self):
+        self.assertEqual(Amenity(), type(Amenity()))
+
+    def test_id_str(self):
+        self.assertEqual(str, type(Amenity.id))
+
+    def test_uniq_id(self):
+        am1 = Amenity()
+        am2 = Amenity()
+        self.assertNotEqual(am1.id, am2.id)
+
+    def deff_created_at(self):
+        am1 = Amenity()
+        sleep(1)
+        am2 = Amenity()
+        self.assertNotEqual(am1.created_at, am2.craeted_at)
+
+    def deff_updated_at(self):
+        am1 = Amenity()
+        sleep(1)
+        am2 = Amenity()
+        self.assertNotEqual(am1.updated_at, am2.updated_at)
+
+if __name__ == "__main__":
+    unittest.main()

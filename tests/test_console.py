5e5cfcb2cffe4b6b2be06b1146d0c5a0f6b36d1f
ZHAYRI123 <merykozan@gmail.com>
2023-12-11 10:56:20 -0800

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,30 +11,26 @@ from unittest.mock import patch
 class TestConsole(unittest.TestCase):
     """ Testing the console functions """
     def test_show(self):
-<<<<<<< HEAD
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show")
+            HBNBCommand().onecmd(" help show")
             a = 'prints the string representation of an instance\n'
             self.assertEqual(a, f.getvalue())
-=======
-        pass
->>>>>>> b547210cd43fbc9efdd5d76ff3ad0a46de939102
 
     def test_clear(self):
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("clear")
+            HBNBCommand().onecmd("help clear")
             a = 'clear the screen\n'
             self.assertEqual(a, f.getvalue())
 
     def test_destroy(self):
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("destroy")
+            HBNBCommand().onecmd("help destroy")
             a = 'Deletes an instance based on the class name and id\n'
             self.assertEqual(a, f.getvalue())
 
     def test_update(self):
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update")
+            HBNBCommand().onecmd("help update")
             a = 'Updates the class\n'
             self.assertEqual(a, f.getvalue())
 

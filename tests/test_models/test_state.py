7341679faf69feffbcc91413cac2951173784104
mistidevs-alx <mistidevs@gmail.com>
2023-12-11 01:06:14 -0800

    test_state file

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Def the unittest for the State class"""
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+from time import sleep
+import os
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """ Testing the State Class """
+    def test_name(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

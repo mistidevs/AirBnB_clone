3433132462625c25d43e3433825ae73202ea035b
ZHAYRI123 <merykozan@gmail.com>
2023-12-12 11:53:53 -0800

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,31 +11,41 @@ from unittest.mock import patch
 
 class TestConsole(unittest.TestCase):
     """ Testing the console functions """
+
+    def test_emptyline(self):
+        HBNBCommand().onecmd("")
+
+    def test_quit(self):
+        self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF:
+        self.assertTrue(HBNBCommand().onecmd("EOF"))
+
     def test_show(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd(" help show")
             a = 'prints the string representation of an instance\n'
             self.assertEqual(a, f.getvalue())
 
-    def test_clear(self):
+    def test_help_clear(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help clear")
             a = 'clear the screen\n'
             self.assertEqual(a, f.getvalue())
 
-    def test_destroy(self):
+    def test_help_destroy(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help destroy")
             a = 'Deletes an instance based on the class name and id\n'
             self.assertEqual(a, f.getvalue())
 
-    def test_update(self):
+    def test_help_update(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help update")
             a = 'Updates the class\n'
             self.assertEqual(a, f.getvalue())
 
-    def test_quit(self):
+    def test_help_quit(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help quit")
             a = 'Quit command to exit the program\n'
